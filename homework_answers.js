82bf79dc3b14eef2fd6d66bc0134c9cf8da2969f
LB3368 <latisha2801@gmail.com>
2022-10-13 15:15:02 -0400

    Yell at the Ninja Turtles answered

--- homework_answers.js
+++ homework_answers.js
@@ -106,17 +106,29 @@ console.log(dart)
 wolfy.name = "Gameboy"
 console.log(wolfy)
 //🔴 The commit message should read:
-//
-console.log('\n\n======================================\n\n')
-/*
-"Wild Wild Life answered"
-Yell at the Ninja Turtles
-Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
-Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
-As a developer, you'll be a lifelong learner and constantly encountering new things. We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own. You've got this!
+//"Wild Wild Life answered"
 
-🔴 The commit message should read:
-"Yell at the Ninja Turtles answered"
+console.log('\n\n======================================\n\n')
+
+
+console.log('\n\n============================Yell at the Ninja Turtles========================\n\n')
+
+//Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Michaelangelo', 'Raphael']
+//Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+for (let turtle of ninjaTurtles) {
+  console.log(turtle.toLocaleUpperCase())
+}
+
+
+
+//As a developer, you'll be a lifelong learner and constantly encountering new things. We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own. You've got this!
+
+//🔴 The commit message should read:
+//"Yell at the Ninja Turtles answered"
+console.log('\n\n==============================\n\n')
+
+/*
 Methods, Revisited
 Here is a list of favMovies:
 
